4912b3f289132b881ef46f16422ad477fb0419b6
TheWorldJar <142929823+TheWorldJar@users.noreply.github.com>
2025-09-10 19:53:38 -0400

    REPL Setup

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,20 @@
+import { createInterface } from "readline";
+
 export function cleanInput(input: string): string[] {
     return input.trim().toLowerCase().split(/\s+/);
+}
+
+export function startREPL() {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > ",
+    });
+    rl.prompt();
+    rl.on("line", (input) => {
+        if (input !== "") {
+            console.log(`Your command was: ${cleanInput(input)[0]}`);
+        }
+        rl.prompt();
+    });
 }
